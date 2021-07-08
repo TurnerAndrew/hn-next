7c08fa88937238eae3c5c3a357680d8511f3f942
Andrew Turner <turner.andrew0@gmail.com>
2021-07-08 15:11:23 -0500

    updated comments functions

--- components/Comments.js
+++ components/Comments.js
@@ -2,28 +2,40 @@ import React from 'react'
 import { useRouter } from 'next/router'
 import { getComment, getStory } from '../functions/stories'
 
-const Comments = (comments) => { 
+const Comments = (comments) => {
   
   const commentIds = comments.comments
-  
+
+  const commentDisplay = []
+   
   const getComment = async (id) => {
     const response = await fetch(`https://hacker-news.firebaseio.com/v0/item/${id}.json`)
     const commentResponse = await response.json()
-    console.log(commentResponse)
+    commentDisplay.push(commentResponse)
   }
 
   const getComments = async () => {
-    const promise = Promise.all(commentIds.map(comment => {
-      getComment(comment).then()
+    const promises = Promise.all(commentIds.map(async comment => {
+      await getComment(comment)
     }))
-    return promise.then(res => console.log(res))
   }
+  
 
   getComments()
 
+  console.log(commentDisplay)
+
+  // const commentsMapped = commentDisplay.map(comment => {
+  //   <div className='comment'>
+  //     <span><p>{comment.by}</p><p>{comment.time}</p></span>
+  //     <span><p>{comment.text}</p></span>
+  //   </div>
+  // })
+
+
   return (
         <div>
-            {/* {commentDisplay} */}
+            {/* {commentsMapped} */}
         </div>
     )
 }
